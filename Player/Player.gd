a760b9289856fe61cf827c92dc092e050e6f9ad9
zacharyranes <zachary.ranes@outlook.com>
2021-05-06 18:19:20 -0400

    starting to track a few ep in

---
+++ Player/Player.gd
@@ -0,0 +1,41 @@
+extends Area2D
+
+var plBullet := preload("res://Bullet/Bullet.tscn")
+
+onready var animatedSprite := $AnimatedSprite
+
+export var speed: float = 100
+var vel := Vector2(0,0)
+
+func _process(delta):
+	if vel.x < 0:
+		animatedSprite.play("Left")
+	elif vel.x > 0:
+		animatedSprite.play("Right")
+	else:
+		animatedSprite.play("Straight")
+	
+	if Input.is_action_pressed("shoot"):
+		var bullet := plBullet.instance()
+		bullet.position = position
+		get_tree().current_scene.add_child(bullet)
+
+func _physics_process(delta):
+	var dirVec := Vector2(0,0)
+	
+	if Input.is_action_pressed("move_left"):
+		dirVec.x = -1
+	elif Input.is_action_pressed("move_right"):
+		dirVec.x = 1
+	if Input.is_action_pressed("move_up"):
+		dirVec.y = -1
+	elif Input.is_action_pressed("move_down"):
+		dirVec.y = 1
+	
+	vel = dirVec.normalized() * speed	
+	position += vel * delta
+	
+	var viewRect := get_viewport_rect()
+#	position.x = clamp(position.x, 0, viewRect.size.x)
+	position.x = clamp(position.x, viewRect.position.x, viewRect.end.x)
+	position.y = clamp(position.y, viewRect.position.y, viewRect.end.y)

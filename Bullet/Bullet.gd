076be32a295c642f5ba066029136f8cbc81de881
zacharyranes <zachary.ranes@outlook.com>
2021-05-07 10:09:58 -0400

    End of EP13

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -5,6 +5,10 @@ export var speed: float = 500
 func _physics_process(delta):
 	position.y -= speed * delta
 
-
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+func _on_Bullet_area_entered(area):
+	if area.is_in_group("damageable"):
+		area.damage(1)
+		queue_free()
--- Meteor/Meteor.gd
+++ Meteor/Meteor.gd
@@ -5,10 +5,13 @@ export var maxSpeed: float = 100
 export var minrotationRate: float = -20
 export var maxrotationRate: float = 20
 
+export var life: int = 20
+
 var speed: float = 0
 var rotationRate: float = 0
 
 func _ready():
+	add_to_group("damageable")
 	speed = rand_range(minSpeed, maxSpeed)
 	rotationRate = rand_range(minrotationRate, maxrotationRate)
 
@@ -16,6 +19,10 @@ func _physics_process(delta):
 	rotation_degrees += rotationRate * delta
 	position.y += speed * delta
 
+func damage(amount: int):
+	life -= amount
+	if life <= 0:
+		queue_free()
+
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
-

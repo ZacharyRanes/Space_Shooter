56e02425797dc56fa8cc8ac9c4b1737570127a37
Zachary Ranes <zachary.ranes@outlook.com>
2021-05-08 20:44:20 -0400

    End of EP21

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,11 +1,18 @@
 extends Area2D
+class_name Enemy
 
 export var verticalSpeed := 10.0
 export var health: int = 10
 
+var playerInArea: Player = null
+
 func _ready():
 	add_to_group("damageable")
-	
+
+func _process(delta):
+	if playerInArea != null:
+		playerInArea.damage(1)
+
 func _physics_process(delta):
 	position.y += verticalSpeed * delta
 
@@ -16,3 +23,11 @@ func damage(amount: int):
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+func _on_Enemy_area_entered(area):
+	if area is Player:
+		playerInArea = area
+
+func _on_Enemy_area_exited(area):
+	if area is Player:
+		playerInArea = null
